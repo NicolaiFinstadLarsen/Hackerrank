eb6d81e55fe956e319e5b1fce857d81bcbbad8cf
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2020-05-09 08:35:19 +0200

    initial commit

--- 2.Basic Data Types/Hackerrank oppgave 5(Lists).py
+++ 2.Basic Data Types/Hackerrank oppgave 5(Lists).py
@@ -1,4 +1,18 @@
-if __name__ == '__main__':
-    N = int(input()  
-        print(type(N))
-        print(N)
+import random
+
+n = random.randint(1,12)
+
+n = int(input())
+
+aList = []
+
+
+i=0
+while i<n: #while loop for å gjøre noe opp til n ganger.
+    #print(n) #bare for meg selv per nå.
+    x = input("aList.")
+    i += 1
+    
+alist.append
+print(aList)
+       
