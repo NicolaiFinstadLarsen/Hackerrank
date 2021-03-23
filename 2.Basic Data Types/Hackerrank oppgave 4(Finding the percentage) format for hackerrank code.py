49e0911021daea06400096c0ade1c4e48d2d425c
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2021-03-23 13:18:22 +0100

    From decimal import Decimal. Finished
    Used Decimal to force two decimals after answer

--- 2.Basic Data Types/Hackerrank oppgave 4(Finding the percentage) format for hackerrank code.py
+++ 2.Basic Data Types/Hackerrank oppgave 4(Finding the percentage) format for hackerrank code.py
@@ -1,4 +1,19 @@
+from decimal import Decimal
+#if __name__ == '__main__':
+    
+    
+#        n = int(input())
+#    student_marks = {}
+#    for _ in range(n):
+#        name, *line = input().split()
+#        scores = list(map(float, line))
+#        student_marks[name] = scores
+#    query_name = input()
+    
+    
+    
     total = float((sum(student_marks[query_name])))
     avg = total / len(student_marks[query_name])
-    avg_float = float(avg)
-    print(avg_float)
+    avg_dec = Decimal(avg)
+    print(round(avg_dec,2))
+
